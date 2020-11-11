f0e63ff2e3b02a12f76f1faea4df06950a19204e
flouheforst <flouheforst>
2020-11-11 19:48:07 +0300

    field send message design

--- client/src/components/FieldSendMessage/FieldSendMessage.jsx
+++ client/src/components/FieldSendMessage/FieldSendMessage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import SendIcon from '@material-ui/icons/Send';
+
 import "./FieldSendMessage.scss";
 
 const FieldSendMessage = () => {
     return(
         <div className="FieldSendMessage">
-            FieldSendMessage
+            <AttachFileIcon className="clip"/>
+            <input placeholder="Write a message..."/>
+            <SentimentSatisfiedIcon className="smile"/>    
+            <SendIcon className="send"/>
         </div>
     )
 }
